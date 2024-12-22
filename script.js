f5f6bb8ef96b10836b5989e9275cbd46cc7d0afc
sumit bharti <sumitbharti230@gmail.com>
2024-12-22 15:42:24 +0545

    first-commit

---
+++ script.js
@@ -0,0 +1,80 @@
+// let arr = [1,2,3,4,5,6]
+// arr.push(6)
+// // console.log(arr)
+// arr.pop()
+// console.log(arr)
+
+// push and pop metbhod
+// let arr1 =[1,2,3,4,5]
+// let arr2 =[100,200]
+// let x = arr1.pop()
+// let y = arr1.pop()
+// arr2.push(x,y)
+
+
+// console.log(arr2)
+// console.log(arr1)
+
+
+// // shift and unshift method
+// let arr = [1,2,3,4,5,6]
+// let x = arr.shift()
+// let y = arr.unshift(10)
+// console.log(arr)
+// console.log(x)
+// console.log(y)
+
+// doing reverse 
+// let arr = [100,200,300]
+// let arr1 =[]
+// let x =arr.pop()
+// let y =arr.pop()
+// let z =arr.pop()
+// arr1.push(x,y,z)
+// console.log(arr1)
+
+// methods for doing reverse and accending order.
+// let x =[5,6,8,2,1]
+// x.sort()
+// x.reverse()
+// console.log(x)
+
+// it is slice methods
+// let arr =[1,2,3,4,5,6,7,8,9]
+// let arr1 = arr.slice(4,7)
+// console.log(arr1)
+
+
+// let x =[1,2,3,4,5,6,4,5,4,8,4,8,4,5,4,4,45,4,5,45,6,4,5,4,65,4,5,5,4,6,4,6,4,1000]
+// // let y = x.slice(3,x.length)//both are same
+// let z = x.slice(3)//both are same
+// console.log(y)
+
+// it is for take out and put another value
+// let arr = [1,2,3,4,5]
+// let y = arr.splice(2,2,300,400,600)
+// console.log(arr)
+
+
+// let arr = [1,2,3,4,5,6,7,8]
+
+// for (let i = 0; i < arr.length;i++)
+// {
+//     if (arr[i] == 5){
+//         console.log(i)
+//     }
+// }
+// console.log(arr.indexOf(5))
+
+
+
+
+
+// this is for checking items is in the array or not if there is items it gives true and if there is not item it gives false
+// let arr = [1,2,3,4,5,6,7,8]
+
+// console.log(arr.includes(5))
+// console.log(arr.includes(100))
+
+
+
